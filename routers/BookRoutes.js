91ba883364dbd85b89e46c2032d313ec702729b7
joao.rodrigues3 <jvmazagao@live.com>
2020-07-29 11:59:36 -0300

    Finishing CRUD

--- routers/BookRoutes.js
+++ routers/BookRoutes.js
@@ -4,33 +4,75 @@ const routes = (model) => {
     const router = express.Router()
 
     router.route('/books')
-    .post((req, res) => {
-        const book = new model(req.body);
-        book.save();
-        res.status(201).send(book);
-    })
-    .get((req, res) => {
-        const query = req.query.genre ? query.genre = req.query.genre : {};
-
-        model.find(query, (err, books) => {
-            if (err) {
-                return res.status(500).send(err);
-            } else {
-                return res.json(books);
-            }
+        .post((req, res) => {
+            const book = new model(req.body);
+            book.save();
+            res.status(201).send(book);
         })
-    });
+        .get((req, res) => {
+            const query = req.query.genre ? query.genre = req.query.genre : {};
 
-    router.route('/books/:id')
-    .get((req, res) => {
+            model.find(query, (err, books) => {
+                if (err) {
+                    return res.status(500).send(err);
+                } else {
+                    return res.json(books);
+                }
+            })
+        });
+
+    router.use('/books/:id', (req, res, next) => {
         model.findById(req.params.id, (err, book) => {
             if (err) {
-                return res.status(500).json(err);
+                res.status(500).send(err);
+            } else if (book) {
+                req.book = book;
+                next();
             } else {
-                return res.send(book)
+                res.status(404).send('Book not found');
             }
         })
-    });
+    })
+
+    router.route('/books/:id')
+        .get((req, res) => {
+            return res.send(req.book)
+        }).put((req, res) => {
+            req.book.title = req.body.title;
+            req.book.author = req.body.author;
+            req.book.genre = req.body.genre
+            req.book.read = req.body.read;
+            req.book.save( (err) => {
+                if(err) {
+                    res.status(500).send(err);
+                } else {
+                    res.json(req.book);
+                }
+            });
+            res.send(req.book);
+        }).patch((req, res) => {
+            if(req.body._id) {
+                delete req.body._id
+            }
+            for(let a in req.body){
+                req.book[a] = req.body[a];
+            } 
+            req.book.save((err) => {
+                if(err) {
+                    res.status(500).send(err);
+                } else {
+                    res.json(req.book);
+                }
+            });
+        }).delete((req, res) => {
+            req.book.remove((err) => {
+                if(err){
+                    res.status(500).send(err);
+                } else {
+                    res.sendStatus(204)
+                }
+            })
+        })
 
     return router;
 }
